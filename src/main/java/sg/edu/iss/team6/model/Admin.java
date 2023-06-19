78543e10ac2af8d2cf5b55d8e268945d6da40a12
usernameissl <sulatt001@e.ntu.edu.sg>
2023-06-19 17:17:16 +0800

    Valiation/Pagination done

--- src/main/java/sg/edu/iss/team6/model/Admin.java
+++ src/main/java/sg/edu/iss/team6/model/Admin.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 import java.io.Serializable;
 
 @Data
@@ -17,14 +20,22 @@ public class Admin implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long adminId;
 
-
     @OneToOne
     @JoinColumn(name = "username")
     private User user;
 
+    @Size(min=2, max=30, message="Name must be 2-30 characters long")
     private String firstName;
+
+    @Size(min=2, max=30, message="Name must be 2-30 characters long")
     private String lastName;
+
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Size(min=2, max=10, message="Contact Number must be 2-10 characters long")
     private String contactNo;
+    
+    @Size(min=2, max=50, message="Address must be 2-30 characters long")
     private String address;
 }
